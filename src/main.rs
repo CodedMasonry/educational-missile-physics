40ab7eccb2a97f47e441474c336b1cf7f617ea46
Brock Shaffer <109301662+CodedMasonry@users.noreply.github.com>
2026-02-19 16:38:59 -0500

    add basic example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use bevy::{pbr::wireframe::WireframePlugin, prelude::*};
+use educational_missile_physics::setup;
 
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins.set(ImagePlugin::default_nearest()),
-            WireframePlugin::default(),
-        ))
+        .add_plugins((DefaultPlugins.set(ImagePlugin::default_nearest()),))
+        .add_systems(Startup, setup)
         .run();
 }
